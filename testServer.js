8d08dcf18d5012b30ace8b940f33251fba2f34df
Daniel <root@ip-172-31-29-53.ec2.internal>
2015-03-17 22:51:00 +0000

    All files for mongo lab

--- testServer.js
+++ testServer.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+var readLIne = require('readline');
 var ROOT_DIR = "html";
 
 var messages = [
@@ -19,13 +19,8 @@ http.createServer(function(req, res){
 	}
 	res.end('\n</body></html>');
 	*/
+
 	var urlObj = url.parse(req.url, true, false);
-	fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data){
-		console.log(ROOT_DIR + urlObj.pathname);
-		console.log("URL path "+urlObj.pathname);
-		console.log("URL search "+urlObj.search);
-		console.log("URL query "+urlObj.query["q"]);	
-		
 		if(urlObj.pathname.indexOf("getcity") != -1)
 		{
 			
@@ -59,11 +54,85 @@ http.createServer(function(req, res){
 			});
 		}
 
-		else
+		else if(urlObj.pathname.indexOf("comment") != -1)
 		{
+			
+			 if(urlObj.pathname.indexOf("comment") != -1){
+				console.log("COMMENT ROUTE");
+				if(req.method === "POST"){
+					console.log("COMMENT POST ROUTE");
+					var jsonData = "";
+					req.on('data', function(chunk){
+						jsonData += chunk;
+						
+					
+					});
+					req.on('end', function(){
+						var reqObj = JSON.parse(jsonData);
+						console.log(reqObj);
+						console.log("Name: " + reqObj.Name);
+						console.log("Comment:" + reqObj.Comment);
+						//Now put it in the database:
+						var mongoClient = require('mongodb').MongoClient;
+						mongoClient.connect("mongodb://localhost/weather", function(err, db){
+							if(err)throw err;
+							db.collection('comments').insert(reqObj, function(err, records){
+								console.log("Record added as " + records[0]._id);
+
+							});
+		
+
+						});
+						
+						res.writeHead(200);
+						res.end("");
+					});
+					
+					
+				}
+				
+				else if(req.method === "GET"){
+					console.log("IN GET");
+					//read teh DB entries:
+					var MongoClient = require('mongodb').MongoClient;
+					MongoClient.connect("mongodb://localhost/weather", function(err, db){
+						if(err)console.log("ERROR in .connect in GET");
+						db.collection("comments", function(err, comments){
+							if(err)console.log("Error in .collection");
+							comments.find(function(err,items){
+								if(err)console.log("erro in .find");
+								items.toArray(function(err, itemArr){
+									if(err)console.log("Error in .toArray");
+									console.log("Documenet array: ");
+									console.log(itemArr);
+									res.writeHead(200);
+									res.end(JSON.stringify(itemArr));
+								});
+							}); 
+							
+						});
+
+
+					});
+					
+						
+				}
+
+
+
+			}
+		}
+		else{
+
+			fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data){
+			console.log(ROOT_DIR + urlObj.pathname);
+			console.log("URL path "+urlObj.pathname);
+			console.log("URL search "+urlObj.search);
+			console.log("URL query "+urlObj.query["q"]);	
+		
 
 			if(err){
-				console.log("ERROR");
+				console.log("ERROR HERE");
 				res.writeHead(404);
 				res.end(JSON.stringify(err));
 				return;
@@ -76,17 +145,14 @@ http.createServer(function(req, res){
 				res.end(data);
 				
 			}
+		});
+	
+
 		}
-	});	
+
+		
 	
 }).listen(80);
-
-var options = {
-	hostname:'localhost',
-	port: '8080',
-	path: '/hello.html'
-
-};
 
 function handleResponse(response){
 	var serverData = '';
